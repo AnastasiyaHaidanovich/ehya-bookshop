7bbeff147afc36639808f0cf858f2bebb45c8c13
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-10-12 13:35:37 +0300

    add: store section and style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // открытие мобильного меню
     var menuButton = document.querySelector(".menu-button")
 
     menuButton.addEventListener('click', function () {
@@ -6,14 +7,44 @@ $(document).ready(function () {
       document.querySelector(".body").classList.toggle("body-scroll");
     })
 
-    
-  $(document).ready(function(){
+    // изменение цвета иконок-сердечек
+    var heartToggle = document.querySelector(".recommend-description__heart");
+
+    heartToggle.addEventListener('click', function () {
+      document.querySelector(".recommend-description__heart").classList.toggle("recommend-description__heart-red");
+ })
+    var heartToggle = document.querySelector(".recommend-description__heart-1");
+
+    heartToggle.addEventListener('click', function () {
+      document.querySelector(".recommend-description__heart-1").classList.toggle("recommend-description__heart-red");
+    })
+    var heartToggle = document.querySelector(".recommend-description__heart-2");
+
+    heartToggle.addEventListener('click', function () {
+      document.querySelector(".recommend-description__heart-2").classList.toggle("recommend-description__heart-red");
+    })
+    var heartToggle = document.querySelector(".recommend-description__heart-3");
+
+    heartToggle.addEventListener('click', function () {
+      document.querySelector(".recommend-description__heart-3").classList.toggle("recommend-description__heart-red");
+    })
+    var heartToggle = document.querySelector(".recommend-description__heart-4");
+
+    heartToggle.addEventListener('click', function () {
+      document.querySelector(".recommend-description__heart-4").classList.toggle("recommend-description__heart-red");
+    })
+    var heartToggle = document.querySelector(".recommend-description__heart-5");
+
+    heartToggle.addEventListener('click', function () {
+      document.querySelector(".recommend-description__heart-5").classList.toggle("recommend-description__heart-red");
+    })
+  
+// плавные якорные ссылки
     $("#menu").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1000);
     });
-});
 
 })
